ddc687aebcae82a37c429412d5ceeb1cded222d3
Dorin Hogea <dhogea@bloomberg.net>
2017-12-06 11:10:03 -0500

    fix cost and remote sql naming (#565)
    * fix cost and remote sql naming
    
    * format
    
    * complimentary test case

--- tests/sc_remsql.test/test_sc.sh
+++ tests/sc_remsql.test/test_sc.sh
@@ -44,6 +44,7 @@ sleep 5
 cdb2sql --cdb2cfg ${a_remcdb2config} $a_remdbname default 'select table_version("t")' >> $output
 
 # retrieve data
+
 cdb2sql -cost --host $mach $a_dbname default "select * from LOCAL_${a_remdbname}.t order by id" >> $output 2>&1
 
 # get the new version V2'
