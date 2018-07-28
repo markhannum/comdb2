bf82df2caef668d78a933e4256c330c365c4b271
Mark Hannum <mhannum72@gmail.com>
2018-07-27 23:14:07 -0400

    Allow online recovery

--- db/db_tunables.c
+++ db/db_tunables.c
@@ -191,6 +191,7 @@ extern int gbl_abort_on_reconstruct_failure;
 extern int gbl_rand_elect_timeout;
 extern int gbl_rand_elect_min_ms;
 extern int gbl_rand_elect_max_ms;
+extern int gbl_online_recovery;
 
 extern long long sampling_threshold;
 
