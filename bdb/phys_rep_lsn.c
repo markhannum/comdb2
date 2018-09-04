9f02229afab95421d33d6bac2d790c7b696a67a6
Mark Hannum <mhannum72@gmail.com>
2018-09-04 16:07:24 -0400

    More fixes to truncate

--- bdb/phys_rep_lsn.c
+++ bdb/phys_rep_lsn.c
@@ -238,10 +238,14 @@ int truncate_log_lock(bdb_state_type* bdb_state, unsigned int file,
     extern int gbl_online_recovery;
     extern int gbl_is_physical_replicant;
     DB_LSN trunc_lsn;
-    char* msg = "truncate log";
+    DBT logrec;
+    DB_LOGC *logc;
+    char *msg = "truncate log";
     int online = gbl_online_recovery;
+    int rc;
 
-    if (flags && bdb_state->repinfo->master_host != bdb_state->repinfo->myhost) {
+    if (flags && bdb_state->repinfo->master_host != 
+            bdb_state->repinfo->myhost) {
         return send_truncate_to_master(bdb_state, file, offset);
     }
 
@@ -251,6 +255,32 @@ int truncate_log_lock(bdb_state_type* bdb_state, unsigned int file,
     } else {
         BDB_WRITELOCK(msg);
     }
+    trunc_lsn.file = file;
+    trunc_lsn.file = offset;
+
+    /* Verify LSN before truncating */
+    rc = bdb_state->dbenv->log_cursor(bdb_state->dbenv, &logc, 0);
+    if (rc) {
+        logmsg(LOGMSG_ERROR, "%s: can't get log cursor rc %d\n", __func__, rc);
+        BDB_RELLOCK();
+        return -1;
+    }
+
+    bzero(&logrec, sizeof(DBT));
+    logrec.flags = DB_DBT_MALLOC;
+    rc = logc->get(logc, &trunc_lsn, &logrec, DB_SET);
+    logc->close(logc, 0);
+
+    if (rc != 0) {
+        logmsg(LOGMSG_ERROR, "%s: error getting %d:%d: %d", __func__, file,
+                offset, rc);
+        BDB_RELLOCK();
+        return -1;
+    }
+
+    if (logrec.data)
+        free(logrec.data);
+
     bdb_state->dbenv->rep_verify_match(bdb_state->dbenv, file, offset, online);
     BDB_RELLOCK();
 
