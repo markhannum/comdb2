7921c0efd385886da80b821e8156480d7d69c053
Mark Hannum <mhannum72@gmail.com>
2019-03-30 13:08:02 -0400

    With 20 columns we get 8 deadlocks reordered, 1500 deadlocks not reordered

--- tests/tools/reorder.c
+++ tests/tools/reorder.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -9,6 +11,7 @@ char *dbname = NULL;
 char *dest = "local";
 int txnsize = 2;
 int iterations = 1000;
+int columns = 2;
 int maxrange = 20000;
 
 void usage(FILE *f)
@@ -18,6 +21,7 @@ void usage(FILE *f)
     fprintf(f, "    -r <maxrange>\n");
     fprintf(f, "    -t <threadcound>\n");
     fprintf(f, "    -x <txnsize>\n");
+    fprintf(f, "    -c <num-columns>\n");
     fprintf(f, "    -i <iterations>\n");
     exit(1);
 }
@@ -25,6 +29,7 @@ void usage(FILE *f)
 void *insert_random(void *x)
 {
     int rc;
+    char *sql = (char *)malloc(64 + (columns * 100));
     cdb2_hndl_tp *hndl = NULL;
     if ((rc = cdb2_open(&hndl, dbname, dest, 0))) {
         fprintf(stderr, "Error opening handle for %s, rc = %d\n", dbname, rc);
@@ -36,9 +41,33 @@ void *insert_random(void *x)
             exit(1);
         }
         for (int j = 0; j < txnsize; j++) {
-            char sql[64];
-            snprintf(sql, sizeof(sql), "insert into t1 (a, b) values(%d, %d)",
-                    rand() % maxrange, rand() % maxrange);
+
+            sql[0] = '\0';
+            strcat(sql, "insert into t1(");
+            for (int c = 0; c < columns ;c++) {
+                char col[6];
+                if (c == 0) {
+                    sprintf(col, "a%d", c);
+                    strcat(sql, col);
+                }
+                else {
+                    sprintf(col, ",a%d", c);
+                    strcat(sql, col);
+                }
+            }
+            strcat(sql, ") values (");
+            for (int c = 0; c < columns ;c++) {
+                char val[12];
+                if (c == 0) {
+                    sprintf(val, "%d", rand() % maxrange);
+                    strcat(sql, val);
+                } else {
+                    sprintf(val, ",%d", rand() % maxrange);
+                    strcat(sql, val);
+                }
+            }
+            strcat(sql, ")");
+
             if ((rc = cdb2_run_statement(hndl, sql))) {
                 fprintf(stderr, "Error running insert, %s, rc = %d\n", cdb2_errstr(hndl), rc);
                 exit(1);
@@ -53,13 +82,58 @@ void *insert_random(void *x)
     return NULL;
 }
 
+int create_table(int columns)
+{
+    int rc;
+    /* "create table t1 (a1, a2, a3, a4, a5..)" */
+    char *sql = (char *)malloc(64 + (columns * 50));
+    cdb2_hndl_tp *hndl = NULL;
+    if ((rc = cdb2_open(&hndl, dbname, dest, 0))) {
+        fprintf(stderr, "Error opening handle for %s, rc = %d\n", dbname, rc);
+        exit(1);
+    }
+
+    sql[0] = '\0';
+    strcat(sql, "create table t1(");
+    for (int i = 0; i < columns; i++) {
+        char colname[20];
+        if (i == 0) {
+            sprintf(colname, "a%d int", i);
+            strcat(sql, colname);
+        } else {
+            sprintf(colname, ",a%d int", i);
+            strcat(sql, colname);
+        }
+    }
+    strcat(sql, ")");
+
+    if ((rc = cdb2_run_statement(hndl, sql))) {
+        fprintf(stderr, "Error running create, %s, %s rc = %d\n", cdb2_errstr(hndl), sql, rc);
+        exit(1);
+    }
+
+    sql[0] = '\0';
+
+    for (int i = 0; i < columns; i++) {
+        sprintf(sql, "create index ix%d on t1(a%d)", i, i);
+        if ((rc = cdb2_run_statement(hndl, sql))) {
+            fprintf(stderr, "Error creating index, %s, rc = %d\n", cdb2_errstr(hndl), rc);
+            exit(1);
+        }
+    }
+
+    cdb2_close(hndl);
+    free(sql);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     int rc, c, errors = 0, threads = 5;
     pthread_t *thds = NULL;
     argv0 = argv[0];
     srand(time(NULL) * getpid());
-    while ((c = getopt(argc, argv, "d:t:x:i:")) != EOF) {
+    while ((c = getopt(argc, argv, "d:t:x:i:c:")) != EOF) {
         switch(c) {
             case 'd':
                 dbname = optarg;
@@ -69,6 +143,9 @@ int main(int argc, char *argv[])
                 break;
             case 'x':
                 txnsize = atoi(optarg);
+                break;
+            case 'c':
+                columns = atoi(optarg);
                 break;
             case 'i':
                 iterations = atoi(optarg);
@@ -90,6 +167,8 @@ int main(int argc, char *argv[])
         cdb2_set_comdb2db_config(conf);
         dest = "default";
     }
+
+    create_table(columns);
 
     thds = (pthread_t *)calloc(sizeof(pthread_t), threads);
     for (int i = 0; i < threads; i++) {
