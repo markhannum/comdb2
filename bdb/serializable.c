511495655d314e79ea7bc75ac095286b398d21bd
Mark Hannum <mhannum72@gmail.com>
2019-07-24 15:55:22 -0400

    commit

--- bdb/serializable.c
+++ bdb/serializable.c
@@ -446,7 +446,9 @@ static int osql_serial_check(bdb_state_type *bdb_state, void *ranges,
             if (!rc)
                 LOGCOPY_32(&rectype, logdta.data);
             else if (rc == DB_NOTFOUND) {
-                break;
+                if (seriallsn.file < curlsn.file || seriallsn.offset <= curlsn.offset) {
+                    abort();
+                }
             } else {
                 fprintf(stderr, "Unable to get last_logical_lsn, rc %d\n", rc);
                 fprintf(stderr, "@ file: %d, offset %d\n", seriallsn.file,
