c2c1cb2cbdc7918e4ad7ff544c0dad05409ea795
Mark Hannum <mhannum72@gmail.com>
2019-07-25 08:28:21 -0400

    only print for the case of records that would have been skipped

--- bdb/serializable.c
+++ bdb/serializable.c
@@ -446,14 +446,15 @@ static int osql_serial_check(bdb_state_type *bdb_state, void *ranges,
             if (!rc)
                 LOGCOPY_32(&rectype, logdta.data);
             else if (rc == DB_NOTFOUND) {
-                if (*file != seriallsn.file ||
-                        *offset != seriallsn.offset) {
-                    *file = seriallsn.file;
-                    *offset = seriallsn.offset;
+                if (*file > seriallsn.file ||
+                        *offset > seriallsn.offset) {
                     logmsg(LOGMSG_USER, "Setting lsn to [%u][%u] rather than "
                             "[%u][%u]\n", *file, *offset, curlsn.file,
                             curlsn.offset);
                 }
+                *file = seriallsn.file;
+                *offset = seriallsn.offset;
+
                 break;
             } else {
                 fprintf(stderr, "Unable to get last_logical_lsn, rc %d\n", rc);
