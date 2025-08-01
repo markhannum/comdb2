79a129f37c91123690a97d6c472c953a249937ac
Mark Hannum <mhannum72@gmail.com>
2025-08-01 09:00:25 -0400

    commit

--- tests/tools/linearizable/jepsen/project.clj
+++ tests/tools/linearizable/jepsen/project.clj
@@ -6,6 +6,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [jepsen "0.1.6"]
                  [org.clojure/java.jdbc "0.6.1"]
+                 [javax.xml.bind/jaxb-api "2.3.1"]
+                 [org.glassfish.jaxb/jaxb-runtime "2.3.1"]
                  [slingshot "0.12.2"]
-                 [com.bloomberg.comdb2/cdb2jdbc "2.0.2"]]
+                 [com.bloomberg.comdb2/cdb2jdbc "2.8.0"]]
+  :jvm-opts ["--add-modules=java.sql"]
   :main comdb2.cli)
